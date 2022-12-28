88f505a0db23c6974b149dab4c428f8858a50adf
TretyakovKonstantin <spb_unex@mail.ru>
2022-12-28 15:58:08 +0300

    Initial commit

---
+++ src/test/java/me/tretyakovv/p3_lesson5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tretyakovv.p3_lesson5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
